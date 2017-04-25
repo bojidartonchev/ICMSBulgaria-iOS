611d024971c6683e2208ebcdb76270f4f9e701b6
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2017-04-25 20:12:07 +0300

    + Loading overlay for loading the data - Package for the all view controllers(code refactor)

--- ICMSBulgaria/ICMSBulgaria/ViewControllers/AllNewsController.cs
+++ ICMSBulgaria/ICMSBulgaria/ViewControllers/AllNewsController.cs
@@ -1,5 +1,6 @@
 ﻿using Foundation;
 using ICMSBulgaria.Models;
+using ICMSBulgaria.Utils;
 using Parse;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,15 @@ namespace ICMSBulgaria
     public partial class AllNewsController : UITableViewController
     {
         private List<News> news;
+        private LoadingOverlay loadingOverlay;
 
         public AllNewsController(IntPtr handle) : base (handle)
         {
+            var bounds = UIScreen.MainScreen.Bounds;
+
+            // show the loading overlay on the UI thread using the correct orientation sizing
+            loadingOverlay = new LoadingOverlay(bounds);
+            View.Add(loadingOverlay);
         }
 
         public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
@@ -50,6 +57,11 @@ namespace ICMSBulgaria
             news = new List<News>(results);
             TableView.Source = new NewsTableSource(news.ToArray());
             TableView.ReloadData();
+
+            if(loadingOverlay != null)
+            {
+                loadingOverlay.Hide();
+            }
         }
     }
 }
